358492242f2f742c681f8d8065489a4c7ab54367
Roman <roma20.94.a@gmail.com>
2022-05-28 15:44:30 +0300

    add adminlte

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
+
